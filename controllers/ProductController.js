1ca5c5eff9056c468954775346357f9f763d91a6
Marcos Allan <allanmenezes880@gmail.com>
2023-12-28 13:12:42 -0300

    ajuste de linha do codigo

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -57,9 +57,7 @@ exports.getPage = async (req, res) => {
     const page = req.query.page || 1
     const limit = 4
     
-    const products = await Product.find()
-    .skip((page - 1) * limit)
-    .limit(limit)
+    const products = await Product.find().skip((page - 1) * limit).limit(limit)
     res.send(products)
 }
 
