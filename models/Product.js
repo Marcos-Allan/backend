a46dc948db5073d22edef546fed414baca2e20d2
Marcos Allan <allanmenezes880@gmail.com>
2024-01-05 10:15:37 -0300

    adicionei dos campos no modelo do mongoDB

--- models/Product.js
+++ models/Product.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose')
 const Product = mongoose.model('Product', {
     price: String,
     descont: String,
-    image: String
+    image: String,
+    description: String,
+    stars: String
 })
 
 module.exports =  Product
