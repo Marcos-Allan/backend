d2e07f958860d27cd8a800ece943874d02511e08
Marcos Allan <allanmenezes880@gmail.com>
2024-01-13 23:42:56 -0300

    teste de websockets com a ws

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const cors = require('cors')
+const http = require('http');
+const WebSocket = require('ws');
 require('dotenv').config()
 
-const app = express()
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
 const port = process.env.PORT || 3000
+
 app.use(express.json())
 app.use(cors())
 
@@ -14,7 +19,24 @@ app.use('/', productRouter)
 
 app.use('/uploads', express.static('uploads'))
 
-app.listen(port, () => {
+// Lógica WebSocket
+wss.on('connection', (ws) => {
+    console.log('WebSocket connected');
+  
+    // Evento de mensagem WebSocket
+    ws.on('message', (message) => {
+      console.log(`WebSocket message received: ${message}`);
+  
+      // Lógica adicional para manipular a mensagem WebSocket, se necessário
+    });
+  
+    // Evento de fechamento WebSocket
+    ws.on('close', () => {
+      console.log('WebSocket disconnected');
+    });
+  });
+
+server.listen(port, () => {
     console.log('server initialized')
     require('./db')
 })
